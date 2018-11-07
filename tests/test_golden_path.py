928cdadf54bdb2dc11ed183e1b28b77a374047a9
vagrant <vagrant@somename>
2018-11-07 00:11:57 +0000

    my first test

---
+++ tests/test_golden_path.py
@@ -0,0 +1,18 @@
+import json
+import pytest
+
+
+import golden_path
+
+
+@pytest.fixture
+def client():
+    golden_path.app.config['TESTING'] = True
+    client = golden_path.app.test_client()
+    yield client
+    
+def test_root_path(client):
+    """Make sure the root path returns {"hello":"world"}."""
+    rv = client.get('/')
+    data = json.loads(rv.data)
+    assert data["hello"] == "world"
